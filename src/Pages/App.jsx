7afb3a42e077491d7379031f91de986f0848162e
Aliaksei Charnykh <ac2oder@gmail.com>
2023-12-07 20:20:10 +0300

    init project

---
+++ src/Pages/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+import Dashboard from "./Main/Main.jsx"
+import Auth from "../Pages/Auth/Auth.jsx"
+
+const App = () => {
+  return (
+    <div>
+        <Dashboard />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Pages/Main/Main.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../../Components/Header/Header.jsx"
+import Footer from "../../Components/Footer/Footer.jsx";
+
+const Dashboard = () => {
+    return (
+        <div>
+            <Header />
+            
+            <Footer />
+        </div>
+    );
+}
+
+export default Dashboard
