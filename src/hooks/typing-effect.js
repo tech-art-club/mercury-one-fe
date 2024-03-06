f9c80a02e4276de7c9556f9ca302a78ec26e0139
Maksym Hudinovych <trade2sale1@gmail.com>
2024-03-06 18:34:41 +0100

    fix

--- src/hooks/typing-effect.js
+++ src/hooks/typing-effect.js
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from 'react';
 
 export function useTypingEffect(textToType, interKeyStrokeDurationInMs) {
   const [currentPosition, setCurrentPosition] = useState(0);
